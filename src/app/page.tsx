7d7eb7cb8a27bd5a13b5fda369f14c9177f1da16
Maksim Leali <maksim@MacBook-Pro-di-Maksim.local>
2025-01-14 17:19:27 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { $color, $cssTRBL, $uw } from "@/theme"
+import { $breakPoint, $color, $cssTRBL, $uw } from "@/theme"
 import styled from "styled-components"
 import { RiMailSendLine } from "react-icons/ri";
 import { IoLogoWhatsapp } from "react-icons/io";
@@ -11,12 +11,14 @@ export default function Main() {
   return <Container>
     <Section>
       <Presentation>
+        <div>
 
-        <h1 className="title">Mi presento</h1>
-        <p>Mi chiamo <b>Maksim Leali</b> e sono uno sviluppatore web da ormai <b>8 anni</b></p>
-        <p>Sono specializzato nella realizzazione di <b>siti di presentazione</b>, curati nei dettagli per rappresentare al meglio brand e persone.</p>
-        <p>Ho inoltre sviluppato progetti originali, come siti per <b>giochi tra amici</b>, pagine dedicate a <b>coppie</b>, e piattaforme per <b>piccole realtà locali</b>,
-          sempre con un <b>approccio su misura</b> e <b>orientato alle esigenze del cliente.</b></p>
+          <h1 className="title">Mi presento</h1>
+          <p>Mi chiamo <b>Maksim Leali</b> e sono uno sviluppatore web da ormai <b>8 anni</b></p>
+          <p>Sono specializzato nella realizzazione di <b>siti di presentazione</b>, curati nei dettagli per rappresentare al meglio brand e persone.</p>
+          <p>Ho inoltre sviluppato progetti originali, come siti per <b>giochi tra amici</b>, pagine dedicate a <b>coppie</b>, e piattaforme per <b>piccole realtà locali</b>,
+            sempre con un <b>approccio su misura</b> e <b>orientato alle esigenze del cliente.</b></p>
+        </div>
         <CTA type="button">
           FAMMI UNA PROPOSTA
           <RiMailSendLine />
@@ -26,13 +28,13 @@ export default function Main() {
       <ImageWrapper>
         <ImageContainer>
           <div className="ph" />
-        <ContactBox>
-          <h3>Come contattarmi</h3>
-          <p>Telefono personale: <a target="_blank" href="tel:+393461038526" >+39 3461038526</a></p>
-          <p>E-Mail: <a href="mailto:contact@makso.ne">contacts@makso.me</a></p>
-          <p>Whatsapp: <a href="https://wa.me/+393461038526" target="_blank">3461038526 <IoLogoWhatsapp style={{marginLeft: "5px"}}/></a></p>
-          <p>Telegram: <a href="https://t.me/+393461038526" target="_blank">3461038526 <BsTelegram style={{marginLeft: "5px"}}/></a></p>
-        </ContactBox>
+          <ContactBox>
+            <h3>Come contattarmi</h3>
+            <p>Telefono personale: <a target="_blank" href="tel:+393461038526" >+39 3461038526</a></p>
+            <p>E-Mail: <a href="mailto:contact@makso.ne">contacts@makso.me</a></p>
+            <p>Whatsapp: <a href="https://wa.me/+393461038526" target="_blank">3461038526 <IoLogoWhatsapp style={{ marginLeft: "5px" }} /></a></p>
+            <p>Telegram: <a href="https://t.me/+393461038526" target="_blank">3461038526 <BsTelegram style={{ marginLeft: "5px" }} /></a></p>
+          </ContactBox>
         </ImageContainer>
       </ImageWrapper>
     </Section>
@@ -85,12 +87,34 @@ const Section = styled.section`
     flex-direction: column;
     gap:${$uw(1)};
   }
+  ${$breakPoint(590)}{
+    flex-direction: column-reverse;
+  }
+  ${$breakPoint(590)}{
+    margin-bottom: ${$uw(6)};
+  }
 `
 
 const Presentation = styled.div`
-  width: ${$uw(18)}; 
+  width: ${$uw(22)}; 
   text-align: justify;
-  
+  ${$breakPoint(1024)}{
+    width: ${$uw(22)};
+  }
+  ${$breakPoint(820)}{
+    width: ${$uw(26)};
+    padding-right:${$uw(1)};
+  }
+  ${$breakPoint(820)}{
+    width: 100%;
+    padding-right:0;
+    display: flex;
+    flex-direction: column;
+  }
+  ${$breakPoint(590)}{
+    flex-direction: column-reverse;
+    
+  }
 `
 
 const InfoWrapper = styled.div`
@@ -98,6 +122,14 @@ const InfoWrapper = styled.div`
   display: flex;
   align-items: stretch;
   gap:${$uw(2)};  
+  ${$breakPoint(820)}{
+    flex-direction: column;
+    gap:${$uw(4)};  
+  }
+  ${$breakPoint(500)}{
+    margin-top: ${$uw(4)};
+    gap:${$uw(6)};  
+  }
 `
 
 const Info = styled.div`
@@ -117,6 +149,11 @@ const Info = styled.div`
     border-radius:4px;
     background-color: ${$color('primary-dark')};
   }
+
+  ${$breakPoint(820)}{
+    width: 100%;
+  }
+
 `
 
 
@@ -128,12 +165,40 @@ const ImageWrapper = styled.div`
   justify-content: center;
   align-items: center;
   
-  .ph {
+  
+
+
+  ${$breakPoint(820)}{
+    width: ${$uw(26)};
+    padding-left:${$uw(1)};
+  }
+  ${$breakPoint(590)}{
+      width: 100%;
+      height:unset;
+      padding-left:0;
+    }
+  
+
+    .ph {
     position: relative;
     z-index:2;
     width: ${$uw(15)};
     height: ${$uw(20)};
     background-color: ${$color('glass-dark')};
+    ${$breakPoint(1024)}{
+      width:${$uw(20)};
+      height:${$uw(34)};
+    }
+    ${$breakPoint(820)}{
+      width: ${$uw(25)};
+    }   
+    ${$breakPoint(590)}{
+      width: ${$uw(22)};
+    }   
+    ${$breakPoint(500)}{
+      width: ${$uw(40)};
+      height: calc(60dvh - ${$uw(5)});
+    }   
   }
 `
 
@@ -146,7 +211,25 @@ const ImageContainer = styled.div`
   align-items: center;
   flex-direction: column;
   position: relative;
-    &::before, &::after {
+  
+    ${$breakPoint(930)}{
+     
+     height: ${$uw(40)};
+  }
+  ${$breakPoint(820)}{
+    width: ${$uw(26)};
+  }
+  ${$breakPoint(590)}{
+    width:100%;
+    flex-direction: row-reverse;
+  }
+  ${$breakPoint(500)}{
+    flex-direction: column;
+    height: unset;
+    overflow: hidden;
+    
+  }
+  &::before, &::after {
       display:block;
       content: "";
       position: absolute;
@@ -155,12 +238,27 @@ const ImageContainer = styled.div`
       aspect-ratio: 1;
       animation: changeradius 20s ease-in-out infinite;
       background-color: ${$color('tertiary')};
+      ${$breakPoint(820)}{
+        width:${$uw(24)};
+        animation: changeradiusMid 20s ease-in-out infinite;
+      }
+      ${$breakPoint(590)}{
+        margin-left: 50%;
+      }
+      ${$breakPoint(500)}{
+        margin-left: 0;
+        width:${$uw(46)};
+        animation: changeradiusMin 20s ease-in-out infinite;
+      }
+      
     }
     &::before {
       animation-delay: 1s;
       background-color: ${$color('tertiary-shade')};
       transform: rotate(90deg);
     }
+
+  
 `
 
 const ContactBox = styled.div`
@@ -172,13 +270,42 @@ const ContactBox = styled.div`
   border-radius: 4px;
   display: flex;
   flex-direction: column;
+ 
+  box-shadow: ${$uw(.2)} ${$uw(.2)} ${$color('secondary-dark')};
+  
+  ${$breakPoint(1024)}{
+    width:${$uw(20)};
+  }
+  ${$breakPoint(820)}{
+    width: ${$uw(25)};
+  }
+  ${$breakPoint(820)}{
+    width: ${$uw(25)};
+  }
+  ${$breakPoint(500)}{
+    width: 100%;
+    box-shadow: ${$uw(.4)} ${$uw(.4)} ${$color('secondary-dark')};
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
   h3 {
     margin-bottom:${$uw(.5)};
+    ${$breakPoint(500)}{
+      margin-bottom:${$uw(2)};
+      
+    }
   }
   p {
     margin-bottom:${$uw(.2)};
+    &:last-child{
+      margin-bottom: 0;
+    }
+    ${$breakPoint(500)}{
+      margin-bottom:${$uw(1)};
+
+    }
   }
-  box-shadow: ${$uw(.2)} ${$uw(.2)} ${$color('secondary-dark')};;
+
 `
 
 const CTA = styled.button`
@@ -194,15 +321,23 @@ const CTA = styled.button`
     margin-top: ${$uw(1)};
     position: relative;
     cursor: pointer;
-    &::after{
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      z-index: -1;
-      top: ${$uw(.2)};
-      left: ${$uw(.2)};
-      content: "";
-      border-radius:4px;
-      background-color: ${$color('secondary-shade')};
+    box-shadow:  ${$uw(.2)} ${$uw(.2)} ${$color('secondary-shade')};
+  ${$breakPoint(590)}{
+    width: ${$uw(22)};
+    position: relative;
+    margin-left: auto;
+    margin-right: ${$uw(1.5)};
+    top: ${$uw(-4)}
   }
+  ${$breakPoint(500)}{
+    width:100%;;
+    padding:${$uw(2)};
+    z-index: 2;
+    top: ${$uw(-1)};
+    border-top-right-radius: 0;
+    border-top-left-radius: 0;
+    box-shadow:  ${$uw(.4)} ${$uw(.4)} ${$color('secondary-shade')};
+    margin-bottom: ${$uw(6)};
+  }
+  
 `
